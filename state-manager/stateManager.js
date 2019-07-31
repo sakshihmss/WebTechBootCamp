233ebf7dd5b935fcd63926e60ef0e72bb396d297
Sakshi <sakhi691998@gmail.com>
2019-07-31 14:43:23 +0530

    Create stateManager.js

---
+++ state-manager/stateManager.js
@@ -0,0 +1,36 @@
+let SM = (function(){
+	let _currentState = undefined,
+	_subscriptionFns = [],
+	_reducer = undefined,
+	_init_action = '@@INIT_ACTION'
+
+function getState(){
+	return _currentState;
+}
+
+function subscribe(subscriptionFn){
+	_subscriptionFns.push(subscriptionFn);
+}
+
+function triggerChange(){
+	_subscriptionFns.forEach(subscriptionFn => subscriptionFn());
+}
+
+function dispatch(action){
+	let newState = _reducer(_currentState, action);
+	if(newState === _currentState)
+		return;
+	_currentState = newState;
+	triggerChange();
+}
+
+function createStore(reducer){
+	_reducer = reducer;
+	_currentState = reducer(_currentState, _init_action);
+	return {getState, subscribe, dispatch};
+}
+return {createStore};
+})();
+
+
+
