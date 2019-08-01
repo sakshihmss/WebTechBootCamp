a739d6dc342f9ce683daf6c03567c600ba9ffd6a
Sakshi <sakhi691998@gmail.com>
2019-08-01 11:05:22 +0530

    generalized bindActionCreators

--- state-manager/stateManager.js
+++ state-manager/stateManager.js
@@ -1,36 +1,44 @@
 let SM = (function(){
 	let _currentState = undefined,
-	_subscriptionFns = [],
-	_reducer = undefined,
-	_init_action = '@@INIT_ACTION';
+		_subscriptionFns = [],
+		_reducer = undefined,
+		_init_action = '@@INIT_ACTION';
 
-function getState(){
-	return _currentState;
-}
+	function getState(){
+		return _currentState;
+	}
 
-function subscribe(subscriptionFn){
-	_subscriptionFns.push(subscriptionFn);
-}
+	function subscribe(subcriptionFn){
+		_subscriptionFns.push(subcriptionFn);
+	}
 
-function triggerChange(){
-	_subscriptionFns.forEach(subscriptionFn => subscriptionFn());
-}
+	function triggerChange(){
+		_subscriptionFns.forEach(subcriptionFn => subcriptionFn());
+	}
 
-function dispatch(action){
-	let newState = _reducer(_currentState, action);
-	if(newState === _currentState)
-		return;
-	_currentState = newState;
-	triggerChange();
-}
+	function dispatch(action){
+		let newState = _reducer(_currentState, action);
+		if (newState === _currentState) return;
+		_currentState = newState;
+		triggerChange();
+	}
 
-function createStore(reducer){
-	_reducer = reducer;
-	_currentState = reducer(_currentState, _init_action);
-	return {getState, subscribe, dispatch};
-}
-return {createStore};
+	function createStore(reducer){
+		_reducer = reducer;
+		_currentState = reducer(_currentState, _init_action);
+		return { getState, subscribe, dispatch };
+	}
+
+	function bindActionCreators(actionCreator, dispatch){
+		let dispatchers = {};
+		for(let key in actionCreator){
+			dispatchers[key] = function(...args){
+				let action = actionCreator[key](...args);
+				dispatch(action);
+			}
+		}
+		return dispatchers;
+	}
+
+	return { createStore, bindActionCreators  };
 })();
-
-
-
